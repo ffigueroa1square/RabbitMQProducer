f092448ae1720d0726fafa64fae11f4977747079
Francisco Figueroa <francisco.figueroa@unosquare.com>
2023-07-05 12:03:46 -0700

    single publisher-consumer queue

--- RabbitMQ.Producer/Program.cs
+++ RabbitMQ.Producer/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory
+        {
+            Uri = new Uri("amqp://guest:guest@localhost:5672")
+        };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare("demo-queue",
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+        var message = new { Name = "Producer", Message = "Hello!" };
+        var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+        channel.BasicPublish("", "demo-queue", null, body);
+    }
+}
